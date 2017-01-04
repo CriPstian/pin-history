9dc68afcbfffae9e9edb8153188ad9edb6dedb59
Cristian Popescu <crip.popescu@gmail.com>
2017-01-04 21:35:14 +0200

    port is set

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/index.html'));
 });
-app.listen(8080);
+app..listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
